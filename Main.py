ee60058831ff8d42b8e349e8db82ee46b75f5c03
SLIGHTNING <3157420882@qq.com>
2024-11-30 16:54:17 +0800

    修复有时程序出错时不显示异常直接退出的 BUG

--- Main.py
+++ Main.py
@@ -24,28 +24,25 @@ def main():
     compiledWork = HTTP.getJSON(compiledWorkURL)
     log(INFO, f"成功获取作品 \033[4;32m{workInfo['name']}\033[0m 的编译文件。")
 
-    try:
-        decompiler = {
-            "KITTEN4": KittenWorkDecompiler,
-            "KITTEN3": KittenWorkDecompiler,
-            "KITTEN2": KittenWorkDecompiler,
-            "COCO": CoCoWorkDecompiler,
-        }[workInfo["type"]](workInfo, compiledWork)
-        decompiler.onStart = lambda: log(INFO, f"开始反编译，作品名称：\033[4;32m{workInfo['name']}\033[0m。")
-        def setActorLog(actor):
-            actor.onPrepare = lambda: log(VERBOSE, f"正在准备角色 \033[4;32m{actor.actor['name']}\033[0m……")
-            actor.onPrepareFunction = lambda name: log(VERBOSE, f"正在准备函数 \033[4;32m{name}\033[0m ……")
-            actor.onStart = lambda: log(VERBOSE, f"正在反编译角色 \033[4;32m{actor.actor['name']}\033[0m……")
-            actor.onStartFunction = lambda name: log(VERBOSE, f"正在反编译函数 \033[4;32m{name}\033[0m……")
-        decompiler.onCreateActor = setActorLog
-        decompiler.onPrepareActors = lambda: log(INFO, "正在准备角色……")
-        decompiler.onStartActors = lambda: log(INFO, "正在反编译角色……")
-        decompiler.onWriteWorkInfo = lambda: log(INFO, "正在清理……")
-        decompiler.onClean = lambda: log(INFO, "正在写入作品信息……")
-        decompiler.onFinish = lambda: log(INFO, "反编译完成。")
-        decompiler.start()
-    except:
-        showError("反编译失败。", traceback.format_exc())
+    decompiler = {
+        "KITTEN4": KittenWorkDecompiler,
+        "KITTEN3": KittenWorkDecompiler,
+        "KITTEN2": KittenWorkDecompiler,
+        "COCO": CoCoWorkDecompiler,
+    }[workInfo["type"]](workInfo, compiledWork)
+    decompiler.onStart = lambda: log(INFO, f"开始反编译，作品名称：\033[4;32m{workInfo['name']}\033[0m。")
+    def setActorLog(actor):
+        actor.onPrepare = lambda: log(VERBOSE, f"正在准备角色 \033[4;32m{actor.actor['name']}\033[0m……")
+        actor.onPrepareFunction = lambda name: log(VERBOSE, f"正在准备函数 \033[4;32m{name}\033[0m ……")
+        actor.onStart = lambda: log(VERBOSE, f"正在反编译角色 \033[4;32m{actor.actor['name']}\033[0m……")
+        actor.onStartFunction = lambda name: log(VERBOSE, f"正在反编译函数 \033[4;32m{name}\033[0m……")
+    decompiler.onCreateActor = setActorLog
+    decompiler.onPrepareActors = lambda: log(INFO, "正在准备角色……")
+    decompiler.onStartActors = lambda: log(INFO, "正在反编译角色……")
+    decompiler.onWriteWorkInfo = lambda: log(INFO, "正在清理……")
+    decompiler.onClean = lambda: log(INFO, "正在写入作品信息……")
+    decompiler.onFinish = lambda: log(INFO, "反编译完成。")
+    decompiler.start()
     saveSourceCode(compiledWork, workInfo["type"])
 
 def saveSourceCode(sourceCode, type):
@@ -81,6 +78,9 @@ def saveSourceCode(sourceCode, type):
 
 if __name__ == "__main__":
     try:
-        main()
-    except Exception:
-        pass
+        try:
+            main()
+        except Exception:
+            showError(traceback.format_exc())
+    except KeyboardInterrupt:
+        print("\n程序已退出。")
--- Tool.py
+++ Tool.py
@@ -3,12 +3,12 @@ import random
 def askYesNot(text):
     return input(f"{text}(y/n)：").lower() in {"y", "yes"}
 
-def showError(message, info):
-    print(f"\033[1;7;91m错误\033[0;1;91m：{message}\033[0m")
+def showError(info):
+    last = info.split('\n')[-2]
+    print(f"\033[1;7;91m错误\033[0;1;91m：{last}\033[0m")
     if askYesNot("是否查看更多信息？"):
-        print(info)
+        print(f"\033[0;1;91m{info}\033[0m")
         input("按下回车键退出程序")
-    raise
 
 def randomBlockID():
     return ''.join([random.choice('0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIGKLMNOPQRSTUVWXYZ') for i in range(20)])
